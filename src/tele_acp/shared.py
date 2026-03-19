df861b40b15739506cfc439c5d0ab9a749300c35
Huanan <donghuanan@inston.ltd>
2026-03-19 13:04:42 +0800

    .

--- src/tele_acp/shared.py
+++ src/tele_acp/shared.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from .constant import NAME
+
 
 def get_app_user_default_dir() -> Path:
     """Get the application default directory path."""
     # TODO: respect XDG and other environment variable.
-    share_dir = Path.home() / ".config" / "tele"
+    share_dir = Path.home() / ".config" / NAME
     share_dir.mkdir(parents=True, exist_ok=True)
     return share_dir
